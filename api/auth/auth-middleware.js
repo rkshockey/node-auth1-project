072796ce4b29fbce0508b5e04371c16f7b339bbc
rkshockey <rkshockey@gmail.com>
2021-09-13 17:33:22 -0400

    Deleted comments

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,13 +1,6 @@
 const { findBy } = require('../users/users-model')
 const passwordSchema = require('../../schema/passwordSchema')
-/*
-  If the user does not have a session saved in the server
 
-  status 401
-  {
-    "message": "You shall not pass!"
-  }
-*/
 function restricted(req, res, next) {
   if (req.session.user){
     next()
@@ -16,14 +9,6 @@ function restricted(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body already exists in the database
-
-  status 422
-  {
-    "message": "Username taken"
-  }
-*/
 async function checkUsernameFree(req, res, next) {
   const [user] = await findBy({ username: req.body.username });
   if (user){
@@ -33,14 +18,6 @@ async function checkUsernameFree(req, res, next) {
   }
 }
 
-/*
-  If the username in req.body does NOT exist in the database
-
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
-*/
 async function checkUsernameExists(req, res, next) {
   const [user] = await findBy({ username: req.body.username });
   if (user){
@@ -50,14 +27,6 @@ async function checkUsernameExists(req, res, next) {
   }
 }
 
-/*
-  If password is missing from req.body, or if it's 3 chars or shorter
-
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
-*/
 async function checkPasswordLength(req, res, next) {
   try {
     await passwordSchema.validate(req.body)
--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -6,29 +6,6 @@ const { checkUsernameFree,
 } = require('./auth-middleware');
 const { add, findBy } = require('../users/users-model')
 
-
-/**
-  1 [POST] /api/auth/register { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "user_id": 2,
-    "username": "sue"
-  }
-
-  response on username taken:
-  status 422
-  {
-    "message": "Username taken"
-  }
-
-  response on password three chars or less:
-  status 422
-  {
-    "message": "Password must be longer than 3 chars"
-  }
- */
 router.post(
   '/register', 
   checkPasswordLength,
@@ -45,21 +22,6 @@ router.post(
   }
 )
 
-/**
-  2 [POST] /api/auth/login { "username": "sue", "password": "1234" }
-
-  response:
-  status 200
-  {
-    "message": "Welcome sue!"
-  }
-
-  response on invalid credentials:
-  status 401
-  {
-    "message": "Invalid credentials"
-  }
- */
 router.post('/login', checkUsernameExists, async (req, res, next) => {
   try{
     const {username, password} = req.body;
@@ -75,21 +37,6 @@ router.post('/login', checkUsernameExists, async (req, res, next) => {
   }
 })
 
-/**
-  3 [GET] /api/auth/logout
-
-  response for logged-in users:
-  status 200
-  {
-    "message": "logged out"
-  }
-
-  response for not-logged-in users:
-  status 200
-  {
-    "message": "no session"
-  }
- */
 router.get('/logout', async (req, res, next) => {
   if (req.session.user){
     req.session.destroy(err => {
